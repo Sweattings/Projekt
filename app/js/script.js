66498081c2d08643323d14d704ec0a1788c3fef6
Sweattings <philippfritz808@yahoo.de>
2021-01-27 17:47:06 +0100

    js, scss backend / and new navbars

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello everybody')
+/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+
+function myFunction () {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav () {
+    document.getElementById("myNav").style.width = "0%";
+}
